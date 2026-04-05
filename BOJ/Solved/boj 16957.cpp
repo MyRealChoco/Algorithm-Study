35e81c0fc5b36bfa116279463e57d58fc84530ef
KimMinWoo <melonmozzi2@naver.com>
2026-04-06 00:08:51 +0900

    boj 16957 revised (536ms -> 52ms)

--- BOJ/Solved/boj 16957.cpp
+++ BOJ/Solved/boj 16957.cpp
@@ -12,7 +12,7 @@ int r, c;
 int dx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
 int dy[8] = {0, 0, 1, -1, 1, -1, 1, -1};
 vector<vector<int>> v, dp, re;
-queue<pair<int, int>> q;
+stack<pair<int, int>> st;
 
 int h(int x, int y) {
     return x*1000 + y;
@@ -21,16 +21,20 @@ pair<int,int> rh(int a) {
     return make_pair(a/1000, a%1000);
 }
 
-void bfs(int x, int y) {
+void dfs(int x, int y) {
     int l, rx, ry;
-    while(!q.empty()) q.pop();
-    q.emplace(x, y);
+    st.emplace(x, y);
 
-    while(!q.empty()) {
-        auto [cx, cy] = q.front();
-        q.pop();
+    bool flag = false;
+    while(!st.empty()) {
+        auto [cx, cy] = st.top();
+        if(flag) {
+            dp[cy][cx] = dp[ry][rx];
+            st.pop();
+            continue;
+        }
 
-        l = INT_MAX;
+        l = INT_MAX; ry=cy; rx=cx;
         f(i, 8) {
             int xt = cx + dx[i];
             int yt = cy + dy[i];
@@ -46,14 +50,17 @@ void bfs(int x, int y) {
         if(l == INT_MAX) {
             dp[y][x] = h(cx, cy);
             re[cy][cx]++;
+            st.pop();
+            flag=true;
         } else {
             if(dp[ry][rx] != -1) {
-                dp[y][x] = dp[ry][rx];
+                dp[cy][cx] = dp[ry][rx];
                 auto [nx, ny] = rh(dp[ry][rx]);
                 re[ny][nx]++;
-                break;
+                st.pop();
+                flag=true;
             }
-            else q.emplace(rx, ry);
+            else st.emplace(rx, ry);
         }
     }
 }
@@ -63,7 +70,7 @@ void solve() {
     v.resize(r, vector<int>(c)); dp.resize(r, vector<int>(c, -1)); re.resize(r, vector<int>(c, 0));
     f(i, r) f(j, c) cin >> v[i][j];
 
-    f(i, r) f(j, c) bfs(j, i);
+    f(i, r) f(j, c) dfs(j, i);
 
     f(i, r) {f(j, c) cout << re[i][j] << " "; cout << '\n';}
 }
